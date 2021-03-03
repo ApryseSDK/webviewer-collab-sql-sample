eea6a7dbacc40ff37ae059dfbdbcf86617a3da55
Jackie Huang <jhuang@pdftron.com>
2021-03-03 09:53:03 +0800

    assign different userNames

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
 import CollabClient from '@pdftron/collab-client';
 import WebViewer from '@pdftron/webviewer';
+import faker from 'faker';
 
 const url = `http://localhost:3000`;
 const subscriptionUrl = `ws://localhost:3000/subscribe`;
 const viewerElement = document.getElementById('viewer');
-const currentUser = 'John';
+const currentUser = faker.name.firstName();
+
 const client = new CollabClient({
   url,
   subscriptionUrl,
