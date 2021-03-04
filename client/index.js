182b5b41fbb21af003b090ad057942b1d46cb01d
Jackie Huang <56648207+jiaqihuang-233@users.noreply.github.com>
2021-03-05 00:58:07 +0800

    Fix sample (#4)
    * update README
    
    * add mention resolvers
    
    * set permissions
    
    * upgrade webviewer version
    
    * assign different userNames
    
    * remove permission setting

--- client/index.js
+++ client/index.js
@@ -1,14 +1,17 @@
 import CollabClient from '@pdftron/collab-client';
 import WebViewer from '@pdftron/webviewer';
+import faker from 'faker';
 
 const url = `http://localhost:3000`;
 const subscriptionUrl = `ws://localhost:3000/subscribe`;
 const viewerElement = document.getElementById('viewer');
-const currentUser = 'John';
+const currentUser = faker.name.firstName();
+
 const client = new CollabClient({
   url,
   subscriptionUrl,
 });
+const documentId = 'abcde';
 
 WebViewer(
   {
@@ -21,9 +24,12 @@ WebViewer(
     client.loadDocument(
       'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
       {
-        documentId: 'abcde',
+        documentId,
         filename: 'demo-annotated.pdf',
+        isPublic: true,
       }
     );
+  }).then(() => {
+    client.joinDocument(documentId);
   });
 });
